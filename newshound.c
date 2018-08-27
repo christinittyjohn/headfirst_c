e86debb739235b3eb76b6c618663485ec0420e13
christinittyjohn <inertiaatmost@gmail.com>
2018-08-27 08:46:50 +0530

    Add files via upload

---
+++ newshound.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+
+int main (int argc, char argv[])
+{
+char *feeds[] = {"https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
+"https://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
+"https://timesofindia.indiatimes.com/rssfeeds/5880659.cms"};
+int times = 3;
+char *phrase = argv[1];
+int i;
+for (i = 0; i < times; i++) {
+char var[255];
+sprintf(var, "RSS_FEED=%s", feeds[i]);
+char *vars[] = {var, NULL};
+if(execle("/usr/bin/python", "/usr/bin/python",
+"/home/chris/programs_c/rssgossip.py",phrase,NULL,vars) == -1) {
+fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+return 1;
+}
+}
+return 0;
+}
